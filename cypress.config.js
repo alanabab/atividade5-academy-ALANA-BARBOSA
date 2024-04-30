9b5140e46a8529007ba4faf2f39afaebc7b7bbff
alanabab <166890813+alanabab@users.noreply.github.com>
2024-04-30 16:51:20 -0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://rarocrud-frontend-88984f6e4454.herokuapp.com/",
+    env: {
+      apiBaseUrl: "https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1/"
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
